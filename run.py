506a98407c266ab76f383bbb01b340a8dc1f1b89
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-01-15 15:11:35 +0100

    Change variable name 'line' to 'row'; add docstrings

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ class Player:
         mission_score_penalty = (5 - mission_data.score) * 100
         mission_prognosis_penalty = 100 - mission_data.prognosis
         skill_penalty = 500 - sum([value[1]
-                                  for value in mission_data.crew.values()])*10
+                                 for value in mission_data.crew.values()]) * 10
         trial_run_bonus = 0 if mission_failed_penalty != 0 else (
                 trial_max_runs - trial_runs) * 10
         mission_difficulty_bonus = 0 if mission_failed_penalty != 0 else \
@@ -64,22 +64,22 @@ class Display:
         self.WIDTH         - Max allowed viewport width
         self.BORDER_CHAR   - Character to use for the outer border
         self.INPUT_PROMPT  - Characters to show before the input line
-        self.EMPTY_LINE    - Empty line string with border chars
-        self.ERROR_LINE_NR - Index of the line with error output
-        self.MENU_LINE_NR  - Index of the line with menu elements
-        self.lines         - List of 23 strings containing all screen output
+        self.EMPTY_ROW     - Empty row string with border chars
+        self.ERROR_ROW_NR  - Index of the row with error output
+        self.MENU_ROW_NR   - Index of the row with menu elements
+        self.rows          - List of 23 strings containing all screen output
     ------------
     Methods:
         __init__()       - Initializes all class attributes
         __empty_screen() - Creates a list of strings for an empty terminal
                            with all border chars in place
-        clear()          - Inserts empty lines to clear terminal output
+        clear()          - Inserts empty rows to clear terminal output
         build_screen()   - Formats and builds a list of strings passed from
                            other functions to prepare terminal output
         build_menu()     - Formats and builds menu string and error string
                            to prepare terminal output
         build_input()    - Formats input prompt and calls _draw to draw screen
-        __draw()         - Draws the screen from self.lines attribute
+        __draw()         - Draws the screen from self.rows attribute
     """
     def __init__(self):
         """"
@@ -89,111 +89,187 @@ class Display:
         self.WIDTH = 80
         self.BORDER_CHAR = "▓"
         self.INPUT_PROMPT = '▓▓▓ :: '
-        self.EMPTY_LINE = f'{self.BORDER_CHAR}{" ":<78}{self.BORDER_CHAR}'
-        self.ERROR_LINE_NR = 21
-        self.MENU_LINE_NR = 20
-        self.lines = self.__empty_screen()
+        self.EMPTY_ROW = f'{self.BORDER_CHAR}{" ":<78}{self.BORDER_CHAR}'
+        self.ERROR_ROW_NR = 21
+        self.MENU_ROW_NR = 20
+        self.rows = self.__empty_screen()
 
 
     def __empty_screen(self):
         """
-        Creates a list of lines containing the border chars and empty space.
+        Creates a list of rows containing the border chars and empty space.
 
         Returns:
             list: Empty terminal screen with borders
         """
-        lines = [str(self.BORDER_CHAR*self.WIDTH)]
-        lines.extend([self.EMPTY_LINE for _ in range(self.HEIGHT-5)])
-        lines.extend([self.BORDER_CHAR*self.WIDTH for _ in range(3)])
-        lines.append(str(self.BORDER_CHAR*self.WIDTH))
+        rows = [str(self.BORDER_CHAR * self.WIDTH)]
+        rows.extend([self.EMPTY_ROW for _ in range(self.HEIGHT - 5)])
+        rows.extend([self.BORDER_CHAR * self.WIDTH for _ in range(3)])
+        rows.append(str(self.BORDER_CHAR * self.WIDTH))
         
-        return lines
+        return rows
 
 
     def clear(self, indexes=[i for i in range(1, 19)], is_error=False):
         """
-        Receives indexes to clear in the terminal and overwrites 
+        Receives indexes to clear in the terminal and overwrites them in the 
+        self.row attribute with empty rows.
 
         Args:
-            indexes (list, optional): _description_. Defaults to [i for i in range(1, 19)].
-            is_error (bool, optional): _description_. Defaults to False.
+            indexes (list, optional)  - List with indexes to be cleared.
+                                        Defaults to all rows above the menu.
+            is_error (bool, optional) - States whether the row to clear is
+                                        the error row. Defaults to False.
         """
         if is_error:
-            self.lines[self.ERROR_LINE_NR] = self.BORDER_CHAR * \
-                self.WIDTH
+            self.rows[self.ERROR_ROW_NR] = self.BORDER_CHAR * self.WIDTH
         else:
             for index in indexes:
-                self.lines[index] = self.EMPTY_LINE
+                self.rows[index] = self.EMPTY_ROW
         
         return
 
 
-    def build_screen(self, text=None, line_nr=1, center=False):
+    def build_screen(self, text, row_nr=1, center=False):
+        """
+        Prepares a text for terminal output above the menu row:
+        Receives a string, list, or dictionary, formats its contents,
+        and overwrites the specified rows of self.rows with the contents.
+
+        Args:
+            text (str, list, dict)  - Message to print on screen.
+            row_nr (int, optional)  - ROW index at which to start.
+                                      Defaults to 1.
+            center (bool, optional) - States whether the message should be
+                                      centered on screen. Defaults to False.
+        
+        Local var:
+            result (str)            - String containing one assembled row
+        """
         result = ''
         if text:
-            if type(text) == list:
-                text_len = len(text)
-                for i in range(text_len):
-                    result = f'{self.BORDER_CHAR}{" "*26 + text[i]:<77} {self.BORDER_CHAR}' if center else f'{self.BORDER_CHAR} {text[i]:<77}{self.BORDER_CHAR}'
-                    self.lines[line_nr + i] = result
-
+            # String processing
+            if type(text) == str:
+                result = (f'{self.BORDER_CHAR}{" "}{text:<76}{" "}'
+                         f'{self.BORDER_CHAR}')
+                self.rows[row_nr] = result
+            # List processing
+            elif type(text) == list:
+                for i in range(len(text)):
+                    result = (f'{self.BORDER_CHAR}{" "*26 + text[i]:<78}'
+                              f'{self.BORDER_CHAR}' if center 
+                              else f'{self.BORDER_CHAR}{" "}{text[i]:<76}{" "}'
+                              f'{self.BORDER_CHAR}')
+                    # Fill the final list starting at received row index
+                    self.rows[row_nr + i] = result
+            # Dictionary processing:
+            # The dictionaries passed have the following format:
+            # {key : ['', ..., '']}
             elif type(text) == dict:
-                j = 0
+                # Counter to keep track of current row index
+                k = 0
                 for key, value in text.items():
-                    string = [f'{key}: ']
-                    for v in value:
-                        string.append(v)
-                    text_len = len(string)
-                    for i in range(text_len-1):
-                        if i == 0:
-                            result = f'{self.BORDER_CHAR} {string[i]:<13}{string[i+1]:<64}{self.BORDER_CHAR}'
-                            self.lines[line_nr + i + j] = result
+                    temp_list = [f'{key}: ']
+                    # Make one list containing the key and all values
+                    try:
+                        for val in value:
+                            temp_list.append(val)
+                    except:
+                        print("Internal error: Dictionary value is not a list")
+                        input()
+                    # Make sure key string is aligned to the left while all
+                    # value strings start at column 17
+                    for i in range(1, len(temp_list)):
+                        if i == 1:
+                            result = (f'{self.BORDER_CHAR}{" "}'
+                                      f'{temp_list[0]:<16}{temp_list[i]:<61}'
+                                      f'{self.BORDER_CHAR}')
+                            self.rows[row_nr + k] = result
                         else:
-                            result = f'{self.BORDER_CHAR} {" " * 13}{string[i+1]:<64}{self.BORDER_CHAR}'
-                            self.lines[line_nr + i + j] = result
-                    j += i + 1
-
-            elif type(text) == str:
-                result = f'{self.BORDER_CHAR} {text:<77}{self.BORDER_CHAR}'
-                self.lines[line_nr] = result
-
+                            result = (f'{self.BORDER_CHAR}{" " * 17}'
+                                      f'{temp_list[i]:<61}{self.BORDER_CHAR}')
+                            self.rows[row_nr + i + k - 1] = result
+                    k += i
             else:
+                print("Internal error: text is not str, list, or dict")
+                input()
+                
                 return
-
         else:
+            print("Internal error: text is empty")
+            input()
+            
             return
 
 
     def build_menu(self, text, is_error=False):
-
+        """
+        Prepares the menu row for terminal output:
+        Formats the string and overwrites the specified row index in self.rows.
+        
+        Args:
+            text (str)                - String containing the menu elements, 
+                                        max 76 chars
+            is_error (bool, optional) - States if text is an error message.
+                                        Defaults to False.
+        """
         if is_error:
-            result = f'{self.BORDER_CHAR} {text:>77}{self.BORDER_CHAR}'
-            self.lines[self.ERROR_LINE_NR] = result
-
+            result = f'{self.BORDER_CHAR}{" "}{text:>76}{" "}{self.BORDER_CHAR}'
+            self.rows[self.ERROR_ROW_NR] = result
         else:
-            result = f'{self.BORDER_CHAR} {text:<77}{self.BORDER_CHAR}'
-            self.lines[self.MENU_LINE_NR] = result
+            result = f'{self.BORDER_CHAR}{" "}{text:<76}{" "}{self.BORDER_CHAR}'
+            self.rows[self.MENU_ROW_NR] = result
+        
+        return
+
 
     def build_input(self, prompt=''):
+        """
+        Calls __draw() to draw the terminal and returns a user input prompt.
+        Thus, the screen is only re-drawn whenever user input is required.
+
+        Args:
+            prompt (str, optional) - Prompt to put before the user input.
+                                     Defaults to ''.
+
+        Returns:
+            str: String with user input decoration and prompt
+        """
         self.__draw()
+        
         return self.INPUT_PROMPT + prompt
     
+    
     def __draw(self):
+        """
+        Clears the previous input and draws the new terminal from self.rows.
+        """
         os.system('cls||clear')
-        for line in self.lines:
-            print(f'{line}')
-
-
-# Add logic: generate cadet dict with random skills, run trials and
-# save results in a new dict
+        for row in self.rows:
+            print(f'{row}')
 
 
 class Cadets:
+    """
+    
+    Attributes:
+        self.SKILLS       -
+        self.NAMES        -
+        self.cadets       -
+        self.display      -
+        self.player_name  -
+        
+    Methods:
+        __init__()              -
+        recruit()               -
+        cadet_skill_generator() -
+        
+    """
 
     def __init__(self, display, player_name):
         # The skills which the cadets are being tested for
-        self.SKILLS = ["Diplomacy", "Science",
-                       "Engineering", "Medicine", "Pilot"]
+        self.SKILLS = ["Captain", "Security Chief",
+                       "Engineer", "Doctor", "Pilot"]
         # Selection of names for cadets (TODO: randomize selection!)
         self.NAMES = random.sample(["Cadet Janeway", "Cadet Picard", "Cadet Kirk", "Cadet Kim", "Cadet Paris",
                                     "Cadet Whorf", "Cadet Crusher", "Cadet Torres", "Cadet Spock", "Cadet Troi"], 6)
@@ -210,7 +286,7 @@ class Cadets:
                    '', f'The following cadets have volunteered for the upcoming mission:', '']
         message.extend(textwrap.wrap(
             f'{", ".join(self.NAMES)}', self.display.WIDTH - 4))
-        self.display.build_screen(message, line_nr=2)
+        self.display.build_screen(message, row_nr=2)
 
         return
 
@@ -399,18 +475,20 @@ class Mission:
             print(key)
             cadet_performance = f'{value[0]} has {"succeeded" if value[1] >= mission_parameters[i] else "failed"}'
             self.mission_log[key] = [cadet_performance]
+            print(self.mission_log[key])
+            input()
             # TODO: Move strings into a Google sheet or use gettext module
             match key:
-                case 'Diplomacy':
+                case 'Captain':
                     if mission_parameters[i] >= 7:
                         self.mission_log[key].append(
-                            "This was a real diplomatic crisis!\n")
+                            "This was a real diplomatic crisis!")
                     elif 4 < mission_parameters[i] < 7:
                         self.mission_log[key].append(
-                            "This mission had challenging diplomatic issues.\n")
+                            "This mission had challenging diplomatic issues.")
                     else:
                         self.mission_log[key].append(
-                            "There was only a minor diplomatic issue on this mission.\n")
+                            "There was only a minor diplomatic issue on this mission.")
                     if value[1] >= mission_parameters[i]:
                         self.score += 1
                         self.mission_log[key].append(
@@ -418,7 +496,7 @@ class Mission:
                     else:
                         self.mission_log[key].append(
                             f"Unfortunately, {value[0]} was unable to deal with it.")
-                case 'Medicine':
+                case 'Doctor':
                     if mission_parameters[i] >= 7:
                         self.mission_log[key].append(
                             "This was a real medical crisis! A planet-wide outbreak!")
@@ -435,7 +513,7 @@ class Mission:
                     else:
                         self.mission_log[key].append(
                             f"Unfortunately, {value[0]} was unable to handle the stress of the medical profession.")
-                case 'Science':
+                case 'Security Chief':
                     if mission_parameters[i] >= 7:
                         self.mission_log[key].append(
                             "This was a real scientific crisis!")
@@ -469,7 +547,7 @@ class Mission:
                     else:
                         self.mission_log[key].append(
                             f"Unfortunately, {value[0]} was unable to handle the stress of being a pilot.")
-                case 'Engineering':
+                case 'Engineer':
                     if mission_parameters[i] >= 7:
                         self.mission_log[key].append(
                             "This was a real engineering crisis!")
@@ -486,6 +564,9 @@ class Mission:
                     else:
                         self.mission_log[key].append(
                             f"Unfortunately, {value[0]} was unable to handle the stress of being an engineer.")
+                case _:
+                    print("Internal error: no such role in the crew")
+                    input()
 
             # print(
             #     f'{value[0]} has {"succeeded" if value[1] >= mission_parameters[i] else "failed"}')
@@ -623,7 +704,7 @@ class Menu():
             self.display.clear(is_error=True)
             trial_status = ['Active trial:']
             trial_status.append(f'{self.chosen_skill}:  ')
-            self.display.build_screen(trial_status, line_nr=16)
+            self.display.build_screen(trial_status, row_nr=16)
 
         short_names = [name.split(" ")[1] for name in cadets.NAMES]
         self.texts_lvl4_cadets = ' '.join(
@@ -643,7 +724,7 @@ class Menu():
                 break
 
         trial_status[1] += f'{cadets.NAMES[c1]} vs ...'
-        self.display.build_screen(trial_status, line_nr=16)
+        self.display.build_screen(trial_status, row_nr=16)
 
         while True:
             c2 = input(self.display.build_input(
@@ -661,7 +742,7 @@ class Menu():
                 break
 
         trial_status[1] = trial_status[1][:-3] + f'{cadets.NAMES[c2]}'
-        self.display.build_screen(trial_status, line_nr=16)
+        self.display.build_screen(trial_status, row_nr=16)
         time.sleep(0.5)
 
         self.stay_in_trial_menu = trials.fill_trials(cadets, skill_nr, c1, c2)
@@ -719,7 +800,7 @@ def loading_screen(display, part=1):
             message.extend(textwrap.wrap(
                 'I am Cat, short for "Cadet Assessment Terminal". Since the speech module is currently undergoing a personality adjustment, I ask you to use your keyboard today (if you can remember how).'))
             message.append("")
-            message.extend(textwrap.wrap("As usual, you will be assessing a group of young cadets who have volunteered to go on an important mission. The mission requires a crew, and each role on the crew must be filled with one cadet. Please run a few trials where you let two cadets compete against each other, and note their performance. The sooner you finish the trials, the better, of course. Be as thorough as you need to, but don't miss the deadline!"))
+            message.extend(textwrap.wrap("As usual, you will be assessing a group of young cadets who have volunteered to go on an important mission. The mission requires a crew, and each role on the crew must be filled with one cadet. Please run a few trials where you let two cadets compete against each other, and note their performance. The sooner you finish the trials, the better, of course. Be as thorough as you need to, but don't miss the deadrow!"))
             message.extend(
                 ["", "Don't forget to provide your full name for the log."])
             display.clear()
